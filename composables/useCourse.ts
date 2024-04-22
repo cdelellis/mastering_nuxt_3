207d8018644e9d3e76b2e23115bbe0974fbd9d0b
carsten.delellis <carsten.delellis@delellis.net>
2024-04-22 13:43:54 +0200

    from KubuntuDev 22.04.2024 13:43h

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,14 +1,41 @@
 import courseData from './courseData';
 
+type Lesson = {
+  title: string;
+  slug: string;
+  number: number;
+  downloadUrl: string;
+  videoId: number;
+  text: string;
+  sourceUrl?: string;
+};
+
+type Chapter = {
+  title: string;
+  slug: string;
+  number: number;
+  lessons: Lesson[];
+};
+
+type Course = {
+  chapters: Chapter[];
+  title: string;
+}
+
 export const useCourse = () => {
   return {
     ...courseData,
-    chapters: courseData.chapters.map((chapter) => ({
+    chapters: courseData.chapters.map((chapter) => {
+      const lessons: Lesson[] = chapter.lessons.map(
+        (lesson) => ({
+          ... lesson,
+          path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+        })
+      );
+      return{
       ...chapter,
-      lessons: chapter.lessons.map((lesson) => ({
-        ...lesson,
-        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
-      })),
-    })),
+      lessons,
+      };
+    }),
   };
 };
